c9791a0956e7a93307106496b1a7661a78f6a258
Shih-Kun Huang <skhuang@Shih-KundeMacBook-Pro.local>
2026-06-16 10:29:49 +0800

    Implement stack operations in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <stack>
+#include <string>
 
 int main() {
-    // TODO: solve lab01-stack
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    std::stack<int> st;
+    for (int i = 0; i < n; ++i) {
+        std::string op;
+        std::cin >> op;
+
+        if (op == "push") {
+            int x;
+            std::cin >> x;
+            st.push(x);
+        } else if (op == "pop") {
+            if (st.empty()) {
+                std::cout << -1 << '\n';
+            } else {
+                std::cout << st.top() << '\n';
+                st.pop();
+            }
+        }
+    }
+
     return 0;
 }
